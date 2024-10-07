90c16167896c3c164c01e97fdf0240066f2846e2
YasinMirnia <ya3mirnia@gmail.com>
2024-10-07 20:55:27 +0330

    Exe. (var) - Calculate user's income, expenses, and witing time to buy a car

--- variables.js
+++ variables.js
@@ -16,3 +16,13 @@ let result = 3 + 2 + "7";
 
 // When adding 3 and 2 (Numbers) to "7" (a String), Js performs type coercion. 5 + "7" Should be 57
 console.log(result);
+
+//Calculate user's income, expenses, and witing time to buy a car
+//Salary = 6000$ per month
+//Expenses = 55$ per day
+//Car Price = 66000$
+
+let expensesPerMonth = 55 * 30;
+let monthlySaving = 6000 - expensesPerMonth;
+let witingTime = 66000 / monthlySaving;
+console.log("Witing time:", witingTime);
