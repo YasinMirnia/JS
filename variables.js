daa66ba1d79b4ddbdcabe0fab5ea179789e7b19f
YasinMirnia <ya3mirnia@gmail.com>
2024-10-07 20:43:12 +0330

    Exe. (var) - Type Coercion

--- variables.js
+++ variables.js
@@ -10,3 +10,9 @@ var2 = temp;
 
 console.log("var1:", var1);
 console.log("var2:", var2);
+
+// Type coercion
+let result = 3 + 2 + "7";
+
+// When adding 3 and 2 (Numbers) to "7" (a String), Js performs type coercion. 5 + "7" Should be 57
+console.log(result);
