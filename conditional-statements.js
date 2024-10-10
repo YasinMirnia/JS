fb06fc430bf05b361ad7326ce5a59fd30600c145
YasinMirnia <ya3mirnia@gmail.com>
2024-10-10 22:48:15 +0330

    Exe. (cond.)- Convert number to month and seasons using Switch/Case

--- conditional-statements.js
+++ conditional-statements.js
@@ -26,7 +26,7 @@ if (num1 === 1) {
 } else if (num1 === 12) {
   console.log("Month:", "Isfand");
 } else {
-  console.log("your number is not valid");
+  console.log("your date is not valid");
 }
 
 if (num1 >= 1 && num1 < 4) {
@@ -38,5 +38,49 @@ if (num1 >= 1 && num1 < 4) {
 } else if (num1 >= 10 && num1 <= 12) {
   console.log("season:", "Winter");
 } else {
-  console.log("your number is not valid");
+  console.log("your date is not valid");
+}
+
+//Convert a number to month and season with Switch/case
+const numMonth = 7;
+
+switch (numMonth) {
+  case 1:
+    console.log("Month: Farvardin", "Season: Spring");
+    break;
+  case 2:
+    console.log("Month: Urdibehesht", "Season: Spring");
+    break;
+  case 3:
+    console.log("Month: Khordad", "Season: Spring");
+    break;
+  case 4:
+    console.log("Month: Tir", "Season: Summer");
+    break;
+  case 5:
+    console.log("Month: Mordad", "Season: Summer");
+    break;
+  case 6:
+    console.log("Month: Shahrivar", "Season: Summer");
+    break;
+  case 7:
+    console.log("Month: Mehr", "Season: Fall");
+    break;
+  case 8:
+    console.log("Month: Apan", "Season: Fall");
+    break;
+  case 9:
+    console.log("Month: Azar", "Season: Fall");
+    break;
+  case 10:
+    console.log("Month: Dey", "Season: Winter");
+    break;
+  case 11:
+    console.log("Month: Bahman", "Season: Winter");
+    break;
+  case 12:
+    console.log("Month: Isfand", "Season: Winter");
+    break;
+  default:
+    console.log("your date is not valid");
 }
